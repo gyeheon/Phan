7ce64f33ab12bc04c00ad451a8fa7438750f487f
Gyeheon Jo <gyeheon1234@gmail.com>
2022-02-22 14:38:09 +0900

    muteall, unmuteall, autounmute

--- main.py
+++ main.py
@@ -6,8 +6,29 @@ intents.members = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 
+@bot.command(name='ping')
+async def ping_(ctx):
+    await ctx.reply('pong!')
 
+@bot.command(name='muteall')
+async def muteall_(ctx):
+    channel = ctx.author.voice.channel
+    members = channel.members
+    for i in members:
+        await i.edit(mute=True)
 
+@bot.command(name='unmuteall')
+async def muteall_(ctx):
+    channel = ctx.author.voice.channel
+    members = channel.members
+    for i in members:
+        await i.edit(mute=False)
+
+@bot.event
+async def on_voice_state_update(Member, before, after):
+    if before.channel == None and after.channel != None:
+        print('a')
+        await Member.edit(mute=False)
 
 
 
