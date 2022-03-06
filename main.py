40337e222bb355d161717b93de59a68c6311db1c
Gyeheon Jo <gyeheon1234@gmail.com>
2022-03-06 20:33:44 +0900

    update number auiz

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+from random import random
 from re import S
+from turtle import clear
 import discord
 from discord.ext import commands
+import random
 
 class main_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.num = "0302"
 
-    @commands.command(name='ping')
-    async def ping_(self, ctx):
-        await ctx.reply("pong!")
+        with open('num.txt', 'r') as f:
+            self.num = f.readline()
+
+    def reset(self):
+        string = "0123456789"
+        result = ''
+        for i in range(4):
+            result += random.choice(string)
+        with open('num.txt', 'w') as f:
+            f.write(result)
+            self.num = result
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -18,10 +29,12 @@ class main_cog(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.id == 949698886128271450 and message.author.bot == False:
-            if message.content.isnumeric():
-                if message.content == self.num:
-                    await message.reply("빙고")
-                else:
-                    await message.reply("땡")
+            if message.content.isnumeric() and message.content == self.num:
+                await message.reply("빙고! 숫자가 바뀌었습니다.")
+                self.reset()
             elif message.author.id != 486859609781436447:
                 await message.delete()
+    
+    @commands.command(name='clear')
+    async def clear_(self, ctx, n: int):
+        await ctx.channel.purge(limit=n)
